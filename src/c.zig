34c69cb26eed4fcf2cf41e711b6b9c2cd9af522b
Matt Keeter <matt.j.keeter@gmail.com>
2021-01-06 08:56:12 -0500

    Include cimgui in c.zig

--- src/c.zig
+++ src/c.zig
@@ -7,6 +7,9 @@ pub usingnamespace @cImport({
     @cInclude("wgpu/wgpu.h");
     @cInclude("shaderc/shaderc.h");
 
+    @cDefine("CIMGUI_DEFINE_ENUMS_AND_STRUCTS", "");
+    @cInclude("cimgui/cimgui.h");
+
     @cInclude("extern/rayray.h");
 
     if (builtin.os.tag == .macos) {
